450d3f6a758ffdc0c88e8be8b45aa7da428c3c9a
Lntechnical <82169706+lntechnical2@users.noreply.github.com>
2021-07-13 07:28:34 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,21 @@
+from pyrogram import Client
+import os
+
+TOKEN = os.environ.get("TOKEN", "")
+
+APP_ID = int(os.environ.get("APP_ID", ""))
+
+API_HASH = os.environ.get("API_HASH", "")
+
+if __name__ == "__main__" :
+    plugins = dict(
+        root="plugins"
+    )
+    app = Client(
+        "GTBot",
+        bot_token=TOKEN,
+        api_id=APP_ID,
+        api_hash=API_HASH,
+        plugins=plugins
+    )
+    app.run()
