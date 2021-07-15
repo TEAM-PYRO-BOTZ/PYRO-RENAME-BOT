436a5b252793ed10ab8416e8977d7af32f7a75b0
Lntechnical <82169706+lntechnical2@users.noreply.github.com>
2021-07-15 15:06:39 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -1,1 +1,30 @@
+import pymongo 
+import os
+
+DB_NAME = os.environ.get("DB_NAME","")
+DB_URL = os.environ.get("DB_URL","")
+mongo = pymongo.MongoClient(DB_URL)
+db = mongo[DB_NAME]
+dbcol = db["USER"]
+
+def insert(chat_id):
+            user_id = int(chat_id)
+            user_det = {"_id":user_id,"file_id":None}
+            try:
+            	dbcol.insert_one(user_det)
+            except:
+            	pass
+
+def addthumb(chat_id, file_id):
+	dbcol.update_one({"_id":chat_id},{"$set":{"file_id":file_id}})
+	
+def delthumb(chat_id):
+	dbcol.update_one({"_id":chat_id},{"$set":{"file_id":None}})
+	
+def find(chat_id):
+	id =  {"_id":chat_id}
+	x = dbcol.find(id)
+	for i in x:
+             lgcd = i["file_id"]
+             return lgcd 
 
