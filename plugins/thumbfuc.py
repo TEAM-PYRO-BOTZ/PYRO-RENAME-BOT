d004ef73a8340f1a52f152b5a5802a5b79974f45
Lntechnical <82169706+lntechnical2@users.noreply.github.com>
2021-07-15 15:47:20 +0530

    Create thumbfuc.py

---
+++ plugins/thumbfuc.py
@@ -0,0 +1,23 @@
+from pyrogram import Client, filters
+from helper.database import find, delthumb, addthumb
+
+@Client.on_message(filters.private & filters.command(['viewthumb']))
+async def viewthumb(client,message):
+		thumb = find(int(message.chat.id))
+		if thumb :
+			await app.send_photo(message.chat.id,photo =f"{photo}")
+		else:
+			await message.reply_text("**You dont have any custom Thumbnail**")
+	
+	
+@Client.on_message(filters.private & filters.command(['delthumb']))
+async def removethumb(client,message):
+	delthumb(int(message.chat.id))
+	await message.reply_text("**Custom Thumbnail Deleted **")
+
+@Client.on_message(filters.private & filters.photo)
+async def addthumbs(client,message):
+	file_id = str(message.photo.file_id)
+	addthumb(message.chat.id , file_id)
+	await message.reply_text("Custom thumbnail saved    ✅")
+	
