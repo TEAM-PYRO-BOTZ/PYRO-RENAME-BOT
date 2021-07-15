9935db1d07dc81971702b14e8b76f5a69c5a6bfc
Lntechnical <82169706+lntechnical2@users.noreply.github.com>
2021-07-15 16:13:31 +0530

    Update filedetect.py

--- plugins/filedetect.py
+++ plugins/filedetect.py
@@ -1,4 +1,5 @@
 from pyrogram import Client, filters
+from pyrogram.types import (  InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
 
 @Client.on_message(filters.private & filters.reply)
 async def refunc(client,message):
