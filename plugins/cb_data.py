af7e05c4b33805aee7855d6891bd15ae2203d29e
Lntechnical <82169706+lntechnical2@users.noreply.github.com>
2021-07-15 16:29:19 +0530

    Update cb_data.py

--- plugins/cb_data.py
+++ plugins/cb_data.py
@@ -1,1 +1,18 @@
+from pyrogram import Client, filters
+from pyrogram.types import (  InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
 
+@Client.on_callback_query(filters.regex('cancel'))
+async def cancel(bot,update):
+	try:
+		await update.message.delete()
+	except:
+		return
+@Client.on_callback_query(filters.regex('rename'))
+async def rename(bot,update):
+	user_id = update.message.chat.id
+	date = update.message.date
+	await update.message.delete()
+	await update.message.reply_text("__Please enter the new filename...__",	
+	reply_to_message_id=update.message.reply_to_message.message_id,  
+	reply_markup=ForceReply(True))
+	
