ea849c9dc2be55f90a1033a134808f178a0cd749
Lntechnical <82169706+lntechnical2@users.noreply.github.com>
2021-07-15 19:52:42 +0530

    Update cb_data.py

--- plugins/cb_data.py
+++ plugins/cb_data.py
@@ -45,7 +45,7 @@ async def doc(bot,update):
      user_id = int(update.message.chat.id)
      thumb = find(user_id)
      if thumb:
-     		ph_path = await bot.download_media(photo)
+     		ph_path = await bot.download_media(thumb)
      		Image.open(ph_path).convert("RGB").save(ph_path)
      		img = Image.open(ph_path)
      		img.resize((320, 320))
@@ -101,7 +101,7 @@ async def vid(bot,update):
      user_id = int(update.message.chat.id)
      thumb = find(user_id)
      if thumb:
-     		ph_path = await bot.download_media(photo)
+     		ph_path = await bot.download_media(thumb)
      		Image.open(ph_path).convert("RGB").save(ph_path)
      		img = Image.open(ph_path)
      		img.resize((320, 320))
@@ -156,7 +156,7 @@ async def aud(bot,update):
      user_id = int(update.message.chat.id)
      thumb = find(user_id)
      if thumb:
-     		ph_path = await bot.download_media(photo)
+     		ph_path = await bot.download_media(thumb)
      		Image.open(ph_path).convert("RGB").save(ph_path)
      		img = Image.open(ph_path)
      		img.resize((320, 320))
