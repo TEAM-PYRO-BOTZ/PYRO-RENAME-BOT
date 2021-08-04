d3fa614f57b417a7d2b03ce68de3351750b810e9
Lntechnical <82169706+lntechnical2@users.noreply.github.com>
2021-08-04 16:58:29 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -5,7 +5,7 @@ DB_NAME = os.environ.get("DB_NAME","")
 DB_URL = os.environ.get("DB_URL","")
 mongo = pymongo.MongoClient(DB_URL)
 db = mongo[DB_NAME]
-dbcol = db["USER"]
+dbcol = db["user"]
 
 def insert(chat_id):
             user_id = int(chat_id)
