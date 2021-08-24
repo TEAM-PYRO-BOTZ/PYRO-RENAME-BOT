67f96f9efd7fa307fc9d3e52e41195e606d98463
lokaman chendekar <82169706+lntechnical2@users.noreply.github.com>
2021-08-24 19:47:10 +0530

    Update thumbfuc.py

--- plugins/thumbfuc.py
+++ plugins/thumbfuc.py
@@ -13,7 +13,7 @@ async def viewthumb(client,message):
 @Client.on_message(filters.private & filters.command(['delthumb']))
 async def removethumb(client,message):
 	delthumb(int(message.chat.id))
-	await message.reply_text("**Custom Thumbnail Deleted **")
+	await message.reply_text("**Custom Thumbnail Deleted Successfully**")
 
 @Client.on_message(filters.private & filters.photo)
 async def addthumbs(client,message):
