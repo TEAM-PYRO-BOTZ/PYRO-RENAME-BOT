f244cdc718d20eae08039f39582d96bcc6f2051b
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-04-24 18:40:26 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply, CallbackQuery
 from pyrogram.errors import UserNotParticipant
 import humanize
-form translation import mr
+form Translation import mr
 from helper.database import  insert 
 from bot import BOT_UN, FORCE_SUB
 
