c5e0c110dc4ac9a375d9b9e692a0b4158069a7b9
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-04-24 17:15:28 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,1 +1,10 @@
-
+class script(object):
+    ABOUT_TXT = """🤖 𝘔𝘠 𝘕𝘈𝘔𝘌: {}
+👑 𝘖𝘞𝘕𝘌𝘙: <a href=https://t.me/mr_MKN>𝘔𝘳.𝘔𝘒𝘕 𝘛𝘎</a>
+👨‍💻 𝘋𝘌𝘝𝘌𝘓𝘖𝘗𝘌𝘙: <a href=https://github.com/lntechnical2>𝘐𝘕𝘛𝘌𝘊𝘏𝘕𝘐𝘊𝘈𝘓</a> 
+📕 𝘓𝘐𝘉𝘙𝘈𝘙𝘠: 𝘗𝘠𝘙𝘖𝘎𝘙𝘈𝘔
+✏️ 𝘓𝘈𝘕𝘎𝘜𝘈𝘎𝘌: <a href=https://www.python.org>𝘗𝘠𝘛𝘏𝘖𝘕 3</a>
+💾 𝘋𝘈𝘛𝘈 𝘉𝘈𝘚𝘌: <a href=https://cloud.mongodb.com>𝘔𝘖𝘕𝘎𝘖 𝘋𝘉</a>
+🖥️ 𝘔𝘠 𝘚𝘌𝘙𝘝𝘌𝘙: <a href=https://dashboard.heroku.com>𝘏𝘌𝘙𝘖𝘒𝘜</a>
+📊 𝘉𝘜𝘐𝘓𝘋 𝘚𝘛𝘈𝘛𝘜𝘚: v1.0.1 [ 𝘏𝘌𝘟𝘈 ]
+⚡️ 𝘊𝘙𝘌𝘈𝘋𝘐𝘛𝘚: <a href=https://youtube.com/c/MoTech_YT>𝘔𝘖 𝘛𝘌𝘊𝘏</a> """                
