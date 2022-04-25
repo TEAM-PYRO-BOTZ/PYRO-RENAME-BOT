a89b5796544d15f1f115c1ee9c353e6944842a8c
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-04-25 20:29:21 +0530

    Update Translation.py

--- Translation.py
+++ Translation.py
@@ -3,13 +3,13 @@ class mr(object):
 ╭───────────⍟
 ├🤖 𝘔𝘠 𝘕𝘈𝘔𝘌 : @{}
 ├👑 𝘖𝘞𝘕𝘌𝘙 : <a href=https://t.me/mr_MKN>𝘔𝘳.𝘔𝘒𝘕 𝘛𝘎</a>
-├👨‍💻 𝘋𝘌𝘝𝘌𝘓𝘖𝘗𝘌𝘙 : <a href=https://github.com/lntechnical2>𝘐𝘕𝘛𝘌𝘊𝘏𝘕𝘐𝘊𝘈𝘓</a> 
-├📕 𝘓𝘐𝘉𝘙𝘈𝘙𝘠 : 𝘗𝘠𝘙𝘖𝘎𝘙𝘈𝘔
+├👨‍💻 𝘋𝘌𝘝𝘌𝘓𝘖𝘗𝘌𝘙 : <a href=https://github.com/lntechnical2>𝘐𝘕𝘛𝘌𝘊𝘏𝘕𝘐𝘊𝘈𝘓</a>
+├🖥️ 𝘛𝘜𝘛𝘖𝘙𝘐𝘈𝘓 : <a href=https://youtu.be/7ALYUk-rYMc>𝘔𝘖 𝘛𝘌𝘊𝘏 𝘠𝘛</a>
+├📕 𝘓𝘐𝘉𝘙𝘈𝘙𝘠 : <a href=https://github.com/pyrogram><𝘗𝘠𝘙𝘖𝘎𝘙𝘈𝘔</a>
 ├✏️ 𝘓𝘈𝘕𝘎𝘜𝘈𝘎𝘌 : <a href=https://www.python.org>𝘗𝘠𝘛𝘏𝘖𝘕 3</a>
 ├💾 𝘋𝘈𝘛𝘈 𝘉𝘈𝘚𝘌 : <a href=https://cloud.mongodb.com>𝘔𝘖𝘕𝘎𝘖 𝘋𝘉</a>
-├🖥️ 𝘔𝘠 𝘚𝘌𝘙𝘝𝘌𝘙 : <a href=https://dashboard.heroku.com>𝘏𝘌𝘙𝘖𝘒𝘜</a>
-├📊 𝘉𝘜𝘐𝘓𝘋 𝘚𝘛𝘈𝘛𝘜𝘚 : v1.0.1 [ 𝘏𝘌𝘟𝘈 ]
-├⚡️ 𝘊𝘙𝘌𝘈𝘋𝘐𝘛𝘚 : <a href=https://youtu.be/7ALYUk-rYMc>𝘔𝘖 𝘛𝘌𝘊𝘏</a>              
+├🌀 𝘔𝘠 𝘚𝘌𝘙𝘝𝘌𝘙 : <a href=https://dashboard.heroku.com>𝘏𝘌𝘙𝘖𝘒𝘜</a>
+├📊 𝘉𝘜𝘐𝘓𝘋 𝘚𝘛𝘈𝘛𝘜𝘚 : v3.9.9 [ 𝘏𝘌𝘟𝘈 ]              
 ╰─────────────────────────⍟
                                  """
 
