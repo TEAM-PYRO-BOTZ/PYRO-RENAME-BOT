a69ab156a95b4c3f5fafbf60dd9c41e8a399a738
Mr muha <91768693+MuhaKn@users.noreply.github.com>
2022-05-25 21:44:46 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/MrMKN/RENAMER_BOT-5.git /RENAMER_BOT-5       
+  git clone https://github.com/MuhaKn/RENAMER_LEGEND.git /RENAMER_BOT-5       
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /RENAMER_BOT-5
 fi
 cd /RENAMER_BOT-5
 pip3 install -U -r requirements.txt
-echo "🔥🔥🔥BOT IS STARTING🔥🔥🔥"
+echo "❣️❣️❣️❣️❣️❣️❣️❣️❣️"
 python3 bot.py
