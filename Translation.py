5eb5ac4a13d52e54aa2e11b385067a6c0389d1b9
Mr muha <91768693+MuhaKn@users.noreply.github.com>
2022-05-25 21:53:26 +0530

    Update Translation.py

--- Translation.py
+++ Translation.py
@@ -3,11 +3,8 @@ class mr(object):
 ╭───[**🔅Progress Bar🔅**]───⍟
 │
 ├<b>📁 : {1} | {2}</b>
-│
 ├<b>🚀 : {0}%</b>
-│
 ├<b>⚡ : {3}/s</b>
-│
 ├<b>⏱️ : {4}</b>
 ╰─────────────────⍟"""
 
@@ -31,13 +28,16 @@ class mr(object):
 •> /start a bot and send any picture to automatically set thumbnile.
 •> /delthumb use this command and delete your old thumbnile.
 •> /viewthumb use this command view your current thumbnile.
+
 📑 <b><u>HOW TO SET CUSTOM CAPTION</u></b>
 •> /set_caption - set a custom caption
 •> /see_caption - see your custom caption
 •> /del_caption - delete custom caption
+
 Example:- /set_caption 📕 File Name: {filename}
 💾 Size: {filesize}
 ⏰ Duration: {duration}
+
 ✏️ <b><u>HOW TO RENAME A FILE</u></b>
 •> send any file and click rename option and type new file name and \n send select [ document, video, audio ]👈 choice this.
 ℹ️ 𝗔𝗻𝘆 𝗢𝘁𝗵𝗲𝗿 𝗛𝗲𝗹𝗽 𝗖𝗼𝗻𝘁𝗮𝗰𝘁 :- <a href=https://t.me/MKN_BOTZ_DISCUSSION_GROUP>𝑴𝑲𝑵 𝑩𝑶𝑻𝒁 𝑺𝑼𝑷𝑷𝑶𝑹𝑻 𝑮𝑹𝑶𝑼𝑷</a>
