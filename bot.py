62ea0d0b4c3dab3e26f0c787dda383b83e90acc0
Mr muha <91768693+MuhaKn@users.noreply.github.com>
2022-05-25 21:52:08 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,6 +31,7 @@ class Bot(Client):
     async def start(self):
        await super().start()
        me = await self.get_me()
+       self.mention = me.mention
        self.username = me.username 
        self.force_channel = FORCE_SUB
        if FORCE_SUB:
@@ -41,7 +42,7 @@ class Bot(Client):
             logging.warning(e) 
             logging.warning("Make Sure Bot admin in force sub channel") 
             self.force_channel = None
-       logging.info(f"{me.first_name} Started")
+       logging.info(f"{me.first_name} 𝚂𝚃𝙰𝚁𝚃𝙴𝙳 ⚡️⚡️⚡️")
         
     async def stop(self, *args):
       await super().stop()
