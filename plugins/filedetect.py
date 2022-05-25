2e3cff3abdf7992c7e451d625af5d28c272ccde6
Mr muha <91768693+MuhaKn@users.noreply.github.com>
2022-05-25 22:12:40 +0530

    Update filedetect.py

--- plugins/filedetect.py
+++ plugins/filedetect.py
@@ -11,11 +11,11 @@ async def refunc(client, message):
        file = msg.reply_to_message
        media = file.media
        await reply_message.delete()
-       button = [[InlineKeyboardButton("📁 Documents",callback_data = "upload_document")]]
+       button = [[InlineKeyboardButton("📁 𝙳𝙾𝙲𝚄𝙼𝙴𝙽𝚃𝚂",callback_data = "upload_document")]]
        if str(media) in ["MessageMediaType.VIDEO", "MessageMediaType.DOCUMENT"]:
-           button.append([InlineKeyboardButton("🎥 Video",callback_data = "upload_video")])
+           button.append([InlineKeyboardButton("🎥 𝚅𝙸𝙳𝙴𝙾",callback_data = "upload_video")])
        elif str(media) == "MessageMediaType.AUDIO":
-           button.append([InlineKeyboardButton("🎵 audio",callback_data = "upload_audio")])
+           button.append([InlineKeyboardButton("🎵 𝙰𝙾𝚄𝙳𝙸𝙾",callback_data = "upload_audio")])
        await message.reply_text(
           f"**Select the output file type**\n**Output FileName** :- ```{new_name}```",
           reply_to_message_id=file.id,
