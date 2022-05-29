a4959f8adfedd3504449b64fda97fc24215be685
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-05-30 00:06:37 +0530

    Create txt.py

---
+++ helper/txt.py
@@ -0,0 +1,51 @@
+class mr(object):
+    PROGRESS_BAR = """\n
+╭───[**🔅Progress Bar🔅**]───⍟
+├<b>📁 : {1} | {2}</b>
+├<b>🚀 : {0}%</b>
+├<b>⚡ : {3}/s</b>
+├<b>⏱️ : {4}</b>
+╰─────────────────⍟"""
+
+    ABOUT_TXT = """
+╭───────────⍟
+├🤖 𝙼𝚈 𝙽𝙰𝙼𝙴 : {}
+├👑 𝙾𝚆𝙽𝙴𝚁 : <a href=https://t.me/mr_MKN>𝙼𝚛.𝙼𝙺𝙽 𝚃𝙶</a>
+├😎 𝙳𝙴𝚅𝚂 : @Mhd_rzn & @mr_MKN 
+├👨‍💻 𝙿𝚁𝙾𝙶𝚁𝙰𝙼𝙴𝚁 : <a href=https://github.com/lntechnical2>𝙻𝙽 𝚃𝙴𝙲𝙷.𝚐𝚒𝚝</a>
+├📕 𝙻𝙸𝙱𝚁𝙰𝚁𝚈 : <a href=https://github.com/pyrogram>𝚈𝚁𝙾𝙶𝚁𝙰𝙼</a>
+├✏️ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴 : <a href=https://www.python.org>𝙿𝚈𝚃𝙷𝙾𝙽 3</a>
+├💾 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴 : <a href=https://cloud.mongodb.com>𝙼𝙾𝙽𝙶𝙾 𝙳𝙱</a>
+├🌀 𝙼𝚈 𝚂𝙴𝚁𝚅𝙴𝚁 : <a href=https://dashboard.heroku.com>𝙷𝙴𝚁𝙾𝙺𝚄</a>
+├📊 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚄𝚂 : v3.6.8 [ 𝙼𝙰𝙹𝙾𝚁 ]              
+╰───────────────⍟
+                                """
+    HELP_TXT = """
+🌌 <b><u>HOW TO SET THUMBNILE</u></b>
+  
+•> /start a bot and send any picture to automatically set thumbnile.
+•> /delthumb use this command and delete your old thumbnile.
+•> /viewthumb use this command view your current thumbnile.
+
+📑 <b><u>HOW TO SET CUSTOM CAPTION</u></b>
+•> /set_caption - set a custom caption
+•> /see_caption - see your custom caption
+•> /del_caption - delete custom caption
+
+Example:- /set_caption 📕 File Name: {filename}
+💾 Size: {filesize}
+⏰ Duration: {duration}
+
+✏️ <b><u>HOW TO RENAME A FILE</u></b>
+•> send any file and click rename option and type new file name and \n send select [ document, video, audio ]👈 choice this.
+ℹ️ 𝗔𝗻𝘆 𝗢𝘁𝗵𝗲𝗿 𝗛𝗲𝗹𝗽 𝗖𝗼𝗻𝘁𝗮𝗰𝘁 :- <a href=https://t.me/MKN_BOTZ_DISCUSSION_GROUP>𝑴𝑲𝑵 𝑩𝑶𝑻𝒁 𝑺𝑼𝑷𝑷𝑶𝑹𝑻 𝑮𝑹𝑶𝑼𝑷</a>
+"""
+
+    DEV_TXT = """
+<b><u>Special Thanks & Developers</b></u> 
+
+• ❣️ <a href=https://github.com/lntechnical2>Programmer</a>
+• ❣️ <a href=https://t.me/Mhd_rzn>Support</a>
+• ❣️ <a href=https://t.me/mr_MKN>Support</a>
+• ❣️ <a href=https://t.me/GitHub_noob>Support</a>
+"""
