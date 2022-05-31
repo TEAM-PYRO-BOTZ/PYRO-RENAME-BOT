cebe9271d133ea1f14b45d5ebb272acd898a495d
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-05-31 21:41:03 +0530

    Update pyro.sh

--- pyro.sh
+++ pyro.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/TEAM-PYRO-BOTZ/RENAMER_Ultron.git /RENAMER_Ultron                              #1
+  git clone https://github.com/TEAM-PYRO-BOTZ/Pyro_Renamer-VQ.git /RENAMER_Ultron                              #1
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /RENAMER_Ultron
