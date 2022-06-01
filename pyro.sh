8bb55577011554efd47aac1af2242a93378666e7
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-06-01 20:57:49 +0530

    Update pyro.sh

--- pyro.sh
+++ pyro.sh
@@ -1,6 +1,6 @@
 export TERM=xterm;
 
-git clone "${REPO_URL:-https://github.com/TEAM-PYRO-BOTZ/Pyro_Renamer-VQ}" Pyro_Renamer-VQ         
+git clone "${REPO_URL:-https://github.com/PYRO-HIDDEN/Pyro_Renamer-VQ}" Pyro_Renamer-VQ         
 cd Pyro_Renamer-VQ
 echo "🚀 BOT IS STARTING ❣️";
 python3 bot.py
