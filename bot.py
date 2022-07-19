dfe2665af4315818d17a592ca2a60dba334a1080
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-07-19 22:45:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,14 @@
 import os 
 import logging 
-import logging.config
 from pyrogram import Client 
 
-logging.config.fileConfig("logging.conf")
 logging.getLogger().setLevel(logging.INFO)
-logging.getLogger("pyrogram").setLevel(logging.ERROR) 
-
+ 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 API_ID = int(os.environ.get("API_ID", ""))
 
 API_HASH = os.environ.get("API_HASH", "")
-
-FORCE_SUB = os.environ.get("FORCE_SUB", None)           
 
 class Bot(Client):
 
@@ -32,16 +27,7 @@ class Bot(Client):
        await super().start()
        me = await self.get_me()
        self.mention = me.mention
-       self.username = me.username 
-       self.force_channel = FORCE_SUB
-       if FORCE_SUB:
-         try:
-            link = await self.export_chat_invite_link(FORCE_SUB)
-            self.invitelink = link
-         except Exception as e:
-            logging.warning(e) 
-            logging.warning("Make Sure Bot admin in force sub channel") 
-            self.force_channel = None
+       self.username = me.username        
        logging.info(f"{me.first_name} 𝚂𝚃𝙰𝚁𝚃𝙴𝙳 ⚡️⚡️⚡️")
         
     async def stop(self, *args):
