7b62302116db361b044f5aeb60585ffb706c4207
Mhdrzn <103956396+Mhdrzn@users.noreply.github.com>
2022-09-01 00:38:35 +0530

    Better

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -21,120 +21,69 @@ Repo Link : https://github.com/TEAM-PYRO-BOTZ/PYRO-RENAME-BOT
 License Link : https://github.com/TEAM-PYRO-BOTZ/PYRO-RENAME-BOT/blob/main/LICENSE
 """
 
-import re
 import os
 import time
-import string
-import random
-import asyncio
+import asyncio 
+import logging 
 import datetime
-import traceback
-import aiofiles.os
+from config import ADMIN
 from helper.database import db
 from pyrogram.types import Message
-from pyrogram import Client ,filters
+from pyrogram import Client, filters
 from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, PeerIdInvalid
 
-BROADCAST_AS_COPY = True 
-id_pattern = re.compile(r'^.\d+$')
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+ 
 broadcast_ids = {}
-
-async def send_msg(user_id, message):
-    try:
-        if BROADCAST_AS_COPY is False:
-            await message.forward(chat_id=user_id)
-        elif BROADCAST_AS_COPY is True:
-            await message.copy(chat_id=user_id)
-        return 200, None
-    except FloodWait as e:
-        await asyncio.sleep(e.x)
-        return send_msg(user_id, message)
-    except InputUserDeactivated:
-        return 400, f"{user_id} : deactivated\n"
-    except UserIsBlocked:
-        return 400, f"{user_id} : blocked the bot\n"
-    except PeerIdInvalid:
-        return 400, f"{user_id} : user id invalid\n"
-    except Exception as e:
-        return 500, f"{user_id} : {traceback.format_exc()}\n"
-
-
+ 
 @Client.on_message(filters.command("users") & filters.user(ADMIN))
-async def get_ststs(bot :Client, message: Message):
+async def get_stats(bot :Client, message: Message):
     mr = await message.reply('**𝙰𝙲𝙲𝙴𝚂𝚂𝙸𝙽𝙶 𝙳𝙴𝚃𝙰𝙸𝙻𝚂.....**')
     total_users = await db.total_users_count()
     await mr.edit( text=f"❤️‍🔥 TOTAL USER'S = `{total_users}`")
 
-
-
 @Client.on_message(filters.command("broadcast") & filters.user(ADMIN) & filters.reply)
-async def broadcast_handler(bot: Client,  m: Message):
+async def broadcast_handler(bot: Client, m: Message):
     all_users = await db.get_all_users()
     broadcast_msg = m.reply_to_message
-    while True:
-        broadcast_id = ''.join([random.choice(string.ascii_letters) for i in range(3)])
-        if not broadcast_ids.get(broadcast_id):
-            break
-    out = await m.reply_text(
-        text=f"Broadcast Started! You will be notified with log file when all the users are notified."
-    )
-    start_time = time.time()
-    total_users = await db.total_users_count()
+    sts_msg = await m.reply_text("broadcast started !") 
     done = 0
     failed = 0
     success = 0
-    broadcast_ids[broadcast_id] = dict(
-        total=total_users,
-        current=done,
-        failed=failed,
-        success=success
-    )
-    async with aiofiles.open('broadcast.txt', 'w') as broadcast_log_file:
-        async for user in all_users:
-            sts, msg = await send_msg(
-                user_id=int(user['_id']),
-                message=broadcast_msg
-            )
-            if msg is not None:
-                await broadcast_log_file.write(msg)
-            if sts == 200:
-                success += 1
-            else:
-                failed += 1
-            if sts == 400:
-                await db.delete_user(user['_id'])
-            done += 1
-            if broadcast_ids.get(broadcast_id) is None:
-                break
-            else:
-                broadcast_ids[broadcast_id].update(
-                    dict(
-                        current=done,
-                        failed=failed,
-                        success=success
-                    )
-                )
-    if broadcast_ids.get(broadcast_id):
-        broadcast_ids.pop(broadcast_id)
+    start_time = time.time()
+    total_users = await db.total_users_count()
+    async for user in all_users:
+        sts = await send_msg(user['_id'], broadcast_msg)
+        if sts == 200:
+           success += 1
+        else:
+           failed += 1
+        if sts == 400:
+           await db.delete_user(user['_id'])
+        done += 1
+        if not done % 20:
+           await sts_msg.edit(f"Broadcast in progress:\nnTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nFailed: {failed}")
     completed_in = datetime.timedelta(seconds=int(time.time() - start_time))
-    await asyncio.sleep(2)
-    await out.delete()
-    if failed == 0:
-        await m.reply_text(
-            text=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.",
-            quote=True
-        )
-    else:
-        await m.reply_document(
-            document='broadcast.txt',
-            caption=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.",
-            quote=True
-        )
-    await aiofiles.os.remove('broadcast.txt')
-
-
-
-
-
-
+    await sts_msg.edit(f"Broadcast Completed:\nCompleted in {completed_in}.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nFailed: {failed}")
+           
+async def send_msg(user_id, message):
+    try:
+        await message.copy(chat_id=int(user_id))
+        return 200, None
+    except FloodWait as e:
+        await asyncio.sleep(e.value)
+        return send_msg(user_id, message)
+    except InputUserDeactivated:
+        logger.info(f"{user_id} : deactivated")
+        return 400
+    except UserIsBlocked:
+        logger.info(f"{user_id} : blocked the bot")
+        return 400
+    except PeerIdInvalid:
+        logger.info(f"{user_id} : user id invalid")
+        return 400
+    except Exception as e:
+        logger.error(f"{user_id} : {e}")
+        return 500
+ 
