073ae565a8ae3d02cc710ec1431e5b9ef831d2be
Python TG <100186554+Pythonmoders@users.noreply.github.com>
2022-08-31 22:24:30 +0530

    Update caption.py

--- plugins/caption.py
+++ plugins/caption.py
@@ -1,26 +1,27 @@
 from pyrogram import Client, filters 
-from helper.database import find, addcaption, delcaption 
+from helper.database import db
 
 @Client.on_message(filters.private & filters.command('set_caption'))
 async def add_caption(client, message):
     if len(message.command) == 1:
-       return await message.reply_text("**Give me a caption to set.\n\nExample:- `/set_caption 📕 File Name: {filename}\n\n💾 Size: {filesize}\n\n⏰ Duration: {duration}`**")
+       return await message.reply_text("**__𝙶𝚒𝚟𝚎 𝚖𝚎 𝚊 𝚌𝚊𝚙𝚝𝚒𝚘𝚗 𝚝𝚘 𝚜𝚎𝚝.__\n\n𝙴𝚡𝚊𝚖𝚙𝚕𝚎:- `/set_caption {prefix}{filename}\n\n💾 Size: {filesize}\n\n⏰ Duration: {duration}`**")
     caption = message.text.split(" ", 1)[1]
-    addcaption(int(message.chat.id), caption)
-    await message.reply_text("**Your Caption successfully added ✅**")
+    await db.set_caption(message.from_user.id, caption=caption)
+    await message.reply_text("__**✅ 𝚈𝙾𝚄𝚁 𝙲𝙰𝙿𝚃𝙸𝙾𝙽 𝚂𝚄𝙲𝙲𝙴𝚂𝚂𝙵𝚄𝙻𝙻𝚈 𝚂𝙰𝚅𝙴𝙳**__")
 
+    
 @Client.on_message(filters.private & filters.command('del_caption'))
-async def delete_caption(client, message): 
-    caption = find(int(message.chat.id))[1]
+async def delete_caption(client, message):
+    caption = await db.get_caption(message.from_user.id)  
     if not caption:
-       return await message.reply_text("**You dont have any custom caption**")
-    delcaption(int(message.chat.id))
-    await message.reply_text("**Your caption successfully deleted ✅**")
+       return await message.reply_text("__**😔 𝚈𝙾𝚄 𝙳𝙾𝙽𝚃 𝙷𝙰𝚅𝙴 𝙰𝙽𝚈 𝙲𝙰𝙿𝚃𝙸𝙾𝙽**__")
+    await db.set_caption(message.from_user.id, caption=None)
+    await message.reply_text("__**❌️ 𝚈𝙾𝚄𝚁 𝙲𝙰𝙿𝚃𝙸𝙾𝙽 𝚂𝚄𝙲𝙲𝙴𝚂𝚂𝙵𝚄𝙻𝙻𝚈 𝙳𝙴𝙻𝙴𝚃𝙴𝙳**__")
                                        
 @Client.on_message(filters.private & filters.command('see_caption'))
-async def see_caption(client, message): 
-    caption = find(int(message.chat.id))[1]
+async def see_caption(client, message):
+    caption = await db.get_caption(message.from_user.id)  
     if caption:
-       await message.reply_text(f"<b><u>Your Caption:</b></u>\n\n`{caption}`")
+       await message.reply_text(f"**𝚈𝙾𝚄𝚁 𝙲𝙰𝙿𝚃𝙸𝙾𝙽:-**\n\n`{caption}`")
     else:
-       await message.reply_text("**You dont have any custom caption**")
+       await message.reply_text("__**😔 𝚈𝙾𝚄 𝙳𝙾𝙽𝚃 𝙷𝙰𝚅𝙴 𝙰𝙽𝚈 𝙲𝙰𝙿𝙸𝙾𝙽**__")
