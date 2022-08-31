d91b3793e64a95628b456dbe204bc3603fa64419
Python TG <100186554+Pythonmoders@users.noreply.github.com>
2022-08-31 22:26:26 +0530

    Update thumbfuc.py

--- plugins/thumbfuc.py
+++ plugins/thumbfuc.py
@@ -1,24 +1,24 @@
 from pyrogram import Client, filters
-from helper.database import find, delthumb, addthumb
+from helper.database import db
 
 @Client.on_message(filters.private & filters.command(['viewthumb']))
-async def viewthumb(client,message):
-    thumb = find(int(message.chat.id))[0]
+async def viewthumb(client, message):    
+    thumb = await db.get_thumbnail(message.from_user.id)
     if thumb:
        await client.send_photo(
 	   chat_id=message.chat.id, 
 	   photo=thumb)
     else:
-        await message.reply_text("**You dont have any custom Thumbnail**") 
+        await message.reply_text("😔 __**𝚈𝙾𝚄 𝙳𝙾𝙽𝚃 𝙷𝙰𝚅𝙴 𝙰𝙽𝚈 𝚃𝙷𝚄𝙼𝙱𝙽𝙰𝙸𝙻**__") 
 		
 @Client.on_message(filters.private & filters.command(['delthumb']))
-async def removethumb(client,message):
-    delthumb(int(message.chat.id))
-    await message.reply_text("**Custom Thumbnail Deleted Successfully**")
+async def removethumb(client, message):
+    await db.set_thumbnail(message.from_user.id, thumbnail=None)
+    await message.reply_text("❌️ __**𝚈𝙾𝚄𝚁 𝚃𝙷𝚄𝙼𝙱𝙽𝙰𝙸𝙻 𝚂𝚄𝙲𝙲𝙴𝚂𝚂𝙵𝚄𝙻𝙻𝚈 𝙳𝙴𝙻𝙴𝚃𝙴𝙳**__")
 	
 @Client.on_message(filters.private & filters.photo)
-async def addthumbs(client,message):
-    file_id = str(message.photo.file_id)
-    addthumb(message.chat.id , file_id)
-    await message.reply_text("**Your Custom Thumbnail Saved Successfully** ✅")
+async def addthumbs(client, message):
+    mkn = await message.reply_text("Please Wait ...")
+    await db.set_thumbnail(message.from_user.id, thumbnail=message.photo.file_id)                
+    await mkn.edit("✅️ __**𝚈𝙾𝚄𝚁 𝚃𝙷𝚄𝙼𝙱𝙽𝙰𝙸𝙻 𝚂𝚄𝙲𝙲𝙴𝚂𝚂𝚂𝙵𝚄𝙻𝙻𝚈 𝚂𝙰𝚅𝙴𝙳**__")
 	
