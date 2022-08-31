addeb94eda3f4f918f058e88376e3b5b0e098ab4
Python TG <100186554+Pythonmoders@users.noreply.github.com>
2022-08-31 22:12:07 +0530

    Create database.py

---
+++ helper/database.py
@@ -0,0 +1,57 @@
+import os
+import datetime
+import motor.motor_asyncio
+
+DB_NAME = os.environ.get("DB_NAME","")                                                        
+DB_URL = os.environ.get("DB_URL","")
+
+class Database:
+
+    def __init__(self, uri, database_name):
+        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
+        self.db = self._client[database_name]
+        self.col = self.db.users
+
+    def new_user(self, id):
+        return dict(
+            id=id,
+            join_date=datetime.date.today().isoformat(),                        
+            thumbnail=None,
+            caption=None
+        )
+
+    async def add_user(self, id):
+        user = self.new_user(id)
+        await self.col.insert_one(user)
+
+    async def is_user_exist(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return True if user else False
+
+    async def total_users_count(self):
+        count = await self.col.count_documents({})
+        return count
+
+    async def get_all_users(self):
+        all_users = self.col.find({})
+        return all_users
+
+    async def delete_user(self, user_id):
+        await self.col.delete_many({'id': int(user_id)})
+    
+    async def set_thumbnail(self, id, thumbnail):
+        await self.col.update_one({'id': id}, {'$set': {'thumbnail': thumbnail}})
+
+    async def get_thumbnail(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return user.get('thumbnail', None)
+
+    async def set_caption(self, id, caption):
+        await self.col.update_one({'id': id}, {'$set': {'caption': caption}})
+
+    async def get_caption(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return user.get('caption', None)
+
+
+db = Database(DB_URL, DB_NAME)
