04b06c6b6d80ce60eae921ecbabe49caa72f0182
Mhdrzn <103956396+Mhdrzn@users.noreply.github.com>
2022-09-01 00:05:11 +0530

    configs

---
+++ config.py
@@ -0,0 +1,17 @@
+import re, os
+
+id_pattern = re.compile(r'^.\d+$') 
+
+API_ID = os.environ.get("API_ID", "")
+
+API_HASH = os.environ.get("API_HASH", "")
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+
+FORCE_SUB = os.environ.get("FORCE_SUB", None) 
+
+DB_NAME = os.environ.get("DB_NAME","")     
+
+DB_URL = os.environ.get("DB_URL","")
+ 
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
