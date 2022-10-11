629f425d586c3a593832b110e50d2a6c694ea8e4
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-10-11 19:15:53 +0530

    Update and rename Start.py to start.py

--- plugins/start.py
+++ plugins/start.py
@@ -29,15 +29,9 @@ import humanize
 import random
 from helper.txt import mr
 from helper.database import db
-from helper.utils import not_subscribed 
 from config import START_PIC, FLOOD, ADMIN 
 
-@Client.on_message(filters.private & filters.create(not_subscribed))
-async def is_not_subscribed(client, message):
-    buttons = [[ InlineKeyboardButton(text="📢𝙹𝚘𝚒𝚗 𝙼𝚢 𝚄𝚙𝚍𝚊𝚝𝚎 𝙲𝚑𝚊𝚗𝚗𝚎𝚕📢", url=client.invitelink) ]]
-    text = "**𝚂𝙾𝚁𝚁𝚈 𝙳𝚄𝙳𝙴 𝚈𝙾𝚄𝚁 𝙽𝙾𝚃 𝙹𝙾𝙸𝙽𝙳 𝙼𝚈 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 😔. 𝙿𝙻𝙴𝙰𝚂𝙴 𝙹𝙾𝙸𝙽 𝙼𝚈 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 𝚃𝙾 𝚄𝚂𝙴 𝚃𝙷𝙸𝚂 𝙱𝙾𝚃 🙏 **"
-    await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup(buttons))
-           
+
 @Client.on_message(filters.private & filters.command(["start"]))
 async def start(client, message):
     user = message.from_user
