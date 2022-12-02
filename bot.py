ce168a693d4bde54a2210035dafdf2e4d416cfb4
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-12-02 10:41:28 +0530

    Remove koyeb support
    renamer bot is didn't support in koyeb

--- bot.py
+++ bot.py
@@ -1,9 +1,7 @@
 import logging
 import logging.config
 from pyrogram import Client 
-from config import API_ID, API_HASH, BOT_TOKEN, FORCE_SUB, PORT
-from aiohttp import web
-from plugins.web_support import web_server
+from config import API_ID, API_HASH, BOT_TOKEN, FORCE_SUB
 
 logging.config.fileConfig('logging.conf')
 logging.getLogger().setLevel(logging.INFO)
@@ -36,11 +34,7 @@ class Bot(Client):
          except Exception as e:
             logging.warning(e)
             logging.warning("Make Sure Bot admin in force sub channel")             
-            self.force_channel = None
-       app = web.AppRunner(await web_server())
-       await app.setup()
-       bind_address = "0.0.0.0"
-       await web.TCPSite(app, bind_address, PORT).start()
+            self.force_channel = None      
        logging.info(f"{me.first_name} 𝚂𝚃𝙰𝚁𝚃𝙴𝙳 ⚡️⚡️⚡️")
       
 
