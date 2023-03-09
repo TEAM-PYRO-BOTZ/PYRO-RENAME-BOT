ca9a9012187509a3a9e4ced5d7b7a1544aca28b3
MKN TG <86054899+MrMKN@users.noreply.github.com>
2023-03-09 22:03:02 +0530

    Update F_Sub.py

--- plugins/F_Sub.py
+++ plugins/F_Sub.py
@@ -30,11 +30,7 @@ async def not_subscribed(_, client, message):
     if not client.force_channel:
         return False
     try:             
-        user = await client.get_chat_member(client.force_channel, message.from_user.id)    
-        if user.status == enums.ChatMemberStatus.BANNED:                       
-            return 500
-        else:
-            return False                     
+        user = await client.get_chat_member(client.force_channel, message.from_user.id)                         
     except UserNotParticipant:
         pass
     return True
@@ -42,8 +38,8 @@ async def not_subscribed(_, client, message):
 
 @Client.on_message(filters.private & filters.create(not_subscribed))
 async def forces_sub(client, message):
-    check = await not_subscribed(_, client, message)
-    if check == 500:
+    user = await client.get_chat_member(client.force_channel, message.from_user.id)    
+    if user.status == enums.ChatMemberStatus.BANNED:                                   
         return await client.send_message(message.from_user.id, text="𝚈𝙾𝚄𝚁 𝙰𝚁𝙴 𝙱𝙰𝙽𝙽𝙴𝙳 𝚃𝙾 𝚄𝚂𝙴 𝙼𝙴")                     
     buttons = [[ InlineKeyboardButton(text="📢𝙹𝚘𝚒𝚗 𝙼𝚢 𝚄𝚙𝚍𝚊𝚝𝚎 𝙲𝚑𝚊𝚗𝚗𝚎𝚕📢", url=f"https://t.me/{FORCE_SUB}") ]]
     text = "**𝚂𝙾𝚁𝚁𝚈 𝙳𝚄𝙳𝙴 𝚈𝙾𝚄𝚁 𝙽𝙾𝚃 𝙹𝙾𝙸𝙽𝙳 𝙼𝚈 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 😔. 𝙿𝙻𝙴𝙰𝚂𝙴 𝙹𝙾𝙸𝙽 𝙼𝚈 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 𝚃𝙾 𝚄𝚂𝙴 𝚃𝙷𝙸𝚂 𝙱𝙾𝚃 🙏 **"
