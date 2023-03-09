3e34f414ce9deb0860281822abb3b7d873a65d39
MKN TG <86054899+MrMKN@users.noreply.github.com>
2023-03-09 21:03:52 +0530

    Update utils.py

--- helper/utils.py
+++ helper/utils.py
@@ -80,11 +80,7 @@ async def not_subscribed(_, client, message):
    if not client.force_channel:
       return False
    try:             
-      user = await client.get_chat_member(client.force_channel, message.from_user.id)                        
-      if user.status == enums.ChatMemberStatus.BANNED:                       
-          return await client.send_message(message.from_user.id, text="𝚈𝙾𝚄𝚁 𝙰𝚁𝙴 𝙱𝙰𝙽𝙽𝙴𝙳 𝚃𝙾 𝚄𝚂𝙴 𝙼𝙴")          
-      else:
-          return False 
+      user = await client.get_chat_member(client.force_channel, message.from_user.id)                              
    except UserNotParticipant:
       pass      
    return True
