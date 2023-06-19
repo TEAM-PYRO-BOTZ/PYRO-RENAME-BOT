a9df9623f7a43c58315c2cc058185efe03038c2b
MKN TG <86054899+MrMKN@users.noreply.github.com>
2023-06-20 00:25:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -42,9 +42,4 @@ class Bot(Client):
             except:
                 print("Pʟᴇᴀꜱᴇ Mᴀᴋᴇ Tʜɪꜱ Iꜱ Aᴅᴍɪɴ Iɴ Yᴏᴜʀ Lᴏɢ Cʜᴀɴɴᴇʟ")
 
-    async def stop(self, *args):
-        await super().stop()      
-        print("Bᴏᴛ Sᴛᴏᴩᴩᴇᴅ.. Byᴇ")
-       
-
 Bot().run()
