e7b451284e3ec457c9f5f6feacee0fe5a5588719
Ashutosh15rai <115929606+Ashutosh15rai@users.noreply.github.com>
2023-08-25 21:12:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -42,7 +42,7 @@ class Config(object):
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001802708543"))
 
     # wes response configuration     
-    WEBHOOK = bool(os.environ.get("WEBHOOK", True))
+    WEBHOOK = bool(os.environ.get("WEBHOOK", False))
 
 
 
