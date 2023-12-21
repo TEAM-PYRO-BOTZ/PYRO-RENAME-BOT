0036d6cb5a2334be3d7ff3913465eb5ec8ed3dc6
kingx16 <151035316+kingx16@users.noreply.github.com>
2023-12-21 10:45:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,19 +26,19 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # pyro client config
-    API_ID    = os.environ.get("API_ID", "")
-    API_HASH  = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    API_ID    = os.environ.get("API_ID", "25207236")
+    API_HASH  = os.environ.get("API_HASH", "3bfb93563745195501dd5ad82ba4432a")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6760966134:AAHeawGKJPPi8q7OQu-syLQW78SxEgW9-LI") 
    
     # database config
     DB_NAME = os.environ.get("DB_NAME","pyro-botz")     
-    DB_URL  = os.environ.get("DB_URL","")
+    DB_URL  = os.environ.get("DB_URL","mongodb+srv://KINGBOT:vishnu2004june@cluster0.1wzoyt7.mongodb.net/?retryWrites=true&w=majority")
  
     # other configs
     BOT_UPTIME  = time.time()
-    START_PIC   = os.environ.get("START_PIC", "")
-    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
-    FORCE_SUB   = os.environ.get("FORCE_SUB", "") 
+    START_PIC   = os.environ.get("START_PIC", None)
+    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '1328169569').split()]
+    FORCE_SUB   = os.environ.get("FORCE_SUB", None) 
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", None))
 
     # wes response configuration     
