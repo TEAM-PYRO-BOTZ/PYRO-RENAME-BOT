ef467e767b31ceb2e4b0304e9a31bf4720926aba
Bikash Maity <147458715+CoderGrill@users.noreply.github.com>
2023-12-31 08:43:46 +0530

    Update thumb_&_cap.py

--- plugins/thumb_&_cap.py
+++ plugins/thumb_&_cap.py
@@ -36,6 +36,9 @@ async def viewthumb(client, message):
 		
 @Client.on_message(filters.private & filters.command(['del_thumb', 'delthumb']))
 async def removethumb(client, message):
+    thumb = await db.get_thumbnail(message.from_user.id)
+    if not thumb:
+       return await message.reply_text("😔 __**Yᴏᴜ Dᴏɴ'ᴛ Hᴀᴠᴇ Aɴy Tʜᴜᴍʙɴᴀɪʟ**__")
     await db.set_thumbnail(message.from_user.id, file_id=None)
     await message.reply_text("❌️ __**Tʜᴜᴍʙɴᴀɪʟ Dᴇʟᴇᴛᴇᴅ**__")
 	
@@ -44,5 +47,3 @@ async def addthumbs(client, message):
     mkn = await message.reply_text("Please Wait ...")
     await db.set_thumbnail(message.from_user.id, file_id=message.photo.file_id)                
     await mkn.edit("✅️ __**Tʜᴜᴍʙɴᴀɪʟ Sᴀᴠᴇᴅ**__")
-
-
